96f3c4115e387fe6ef3ecd647d46df1f153f6945
AnkitShaw-100 <ankitshaw6933@gmail.com>
2026-04-20 01:19:28 +0530

    Update CORS origin URL for production environment

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://finora-fullstack.vercel.app",
+      "https://use-finora.vercel.app/",
       "http://localhost:5173",
     ],
     credentials: true,
