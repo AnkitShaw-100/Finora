c5a439d95e5aface09c2c92a602f914f0b53f660
AnkitShaw-100 <ankitshaw6933@gmail.com>
2026-04-20 01:26:38 +0530

    Fix CORS origin URL by removing trailing slash

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://use-finora.vercel.app/",
+      "https://use-finora.vercel.app",
       "http://localhost:5173",
     ],
     credentials: true,
